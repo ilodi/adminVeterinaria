9bd7a25ae83487d865402c12f64df1273ae5541a
David Lopez <lodi.dev@outlook.com>
2019-06-17 23:45:52 -0500

    Creando la app y su carpeta de componenetes al igual de el primer componente header

---
+++ pacientesveterinaria/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './bootstrap.min.css'
+import Header from './conponents/Header'
+
+class App extends Component{
+  state ={
+
+  }
+  render(){
+    return( 
+      <div class="container">
+        <header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ pacientesveterinaria/src/conponents/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () =>(
+    <header>
+        <h1 className="text-center">
+        
+        </h1>
+    </header>
+);
+
+export default Header;
