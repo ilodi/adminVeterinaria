142fa44167dcaa700f26b5bed06d292bdba098ce
David Lopez <lodi.dev@outlook.com>
2019-06-21 23:45:57 -0500

    Ya se estan mostrando en pantalla los elementos

--- pacientesveterinaria/src/App.js
+++ pacientesveterinaria/src/App.js
@@ -1,31 +1,35 @@
-import React, {Component} from 'react';
-import './bootstrap.min.css'
-import Header from './conponents/Header'
-import NuevaCita from './conponents/NuevaCita'
+import React, { Component } from 'react';
+import "./bootstrap.min.css";
+import Header from './components/Header';
+import NuevaCita from './components/NuevaCita';
+import ListaCitas from './components/ListaCitas';
 
-class App extends Component{
-  state ={
-    citas:[]
-  }
-  crearNuevaCita = datos =>{
+class App extends Component {
+  state = {
+    citas: []
+  };
+  crearNuevaCita = datos => {
     //copiar el state actual || Esto es como un push
     const citas = [...this.state.citas, datos];
     //agregar el nuevo state
     this.setState({
       //como se llaman igual no se tiene que escribir de nuevo citas: citas
       citas
-    })
-
-  }
-  render(){
-    return( 
+    });
+  };
+  render() {
+    return (
       <div className="container">
-        <Header title='Administrador Pacientes Veterinaria' />
+        <Header title="Administrador Pacientes Veterinaria" />
         <div className="row">
-        <div className="col-md-10 mx-auto">
-          <NuevaCita crearNuevaCita={this.crearNuevaCita}/>
+          <div className="col-md-10 mx-auto">
+            <NuevaCita crearNuevaCita={this.crearNuevaCita} />
+          </div>
+          <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas citas={this.state.citas} />
+          </div>
         </div>
-        </div>
+
       </div>
     );
   }
--- pacientesveterinaria/src/conponents/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = ({ title }) => (
-  <header>
-    <h1 className="text-center">{title}</h1>
-  </header>
-);
-
-export default Header;
