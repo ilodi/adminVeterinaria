6d0f8a269d69bc57993301dfaf58be0bbd6661b7
David Lopez <lodi@MacBook-Pro-de-David.local>
2019-06-25 00:29:57 -0500

    Con el operador se revisa si hay o no hay citas

--- pacientesveterinaria/src/components/ListaCitas.js
+++ pacientesveterinaria/src/components/ListaCitas.js
@@ -3,11 +3,15 @@ import Cita from './Citas';
 import PropTypes from 'prop-types';
 
 const ListaCitas = ({ citas, elimanarCitas }) => {
+
+  //Imprimir mensaje si hay citas o no
+  const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas aquí' ;
+
  return(
     <div className="card mt-2 py-5">
     <div className="card-body">
       <h2 className="card-title text-center">
-          Administra las citas aquí
+         {mensaje}
       </h2>
       <div className="lista-citas">
         { citas.map(cita => (
